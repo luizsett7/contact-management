1d12e2f16a7c61717d84bfdd70cb01bcdd1c4baf
Luiz Guilherme <luizsett7@gmail.com>
2024-12-13 15:23:22 -0300

    contact migration, model and controller added, routes modified

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts.index', compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:5',
+            'contact' => 'required|digits:9|unique:contacts',
+            'email' => 'required|email|unique:contacts',
+        ]);
+
+        Contact::create($request->all());
+        return redirect()->route('contacts.index')->with('success', 'Contact created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('contacts.show', compact('contact'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('contacts.edit', compact('contact'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        $request->validate([
+            'name' => 'required|min:5',
+            'contact' => 'required|digits:9|unique:contacts,contact,' . $contact->id,
+            'email' => 'required|email|unique:contacts,email,' . $contact->id,
+        ]);
+
+        $contact->update($request->all());
+        return redirect()->route('contacts.index')->with('success', 'Contact updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return redirect()->route('contacts.index')->with('success', 'Contact deleted successfully.');
+    }
+}
